db35430ba98dc24458e477fc6f005205f0a4e8ac
wong <36259838+zwong91@users.noreply.github.com>
2022-09-25 20:52:48 +0800

    att

--- x86_S/tests/att.s
+++ x86_S/tests/att.s
@@ -2,22 +2,21 @@
 .section .text
 .globl main
 main:
-    # 内联汇编
+    # Linus 在汇编程序中直接使用系统调用, 说明as86和gnu at&t 区别
+    movl $4, %eax # write   # 系统调用号, 写操作
+    movl $1, %ebx # stdout  # 参数1 写调用参数fd, 1 == stdout
+    movl $message, %ecx # buffer   # 参数2 缓冲区指针
+    movb $'A', message          # 依次替换buffer中0-4位置元素 %ecx = message = message[0] = 'A'
+    movb $'B', 1(%ecx)          # message[1] = [%ecx + 1] = 'B'
+    movb $'C', (%ecx, %ebx, 2)  # message[2] = [%ecx + %ebx * 2]
+    movb $'D', message - 1(, %ebx, 4)  message[3] = [message + %ebx * 4 - 1]
+    movb $'E', message(, %ebx, 4)   # message[4] = [message + %ebx * 4]
 
-    movl $4, %eax # write
-    movl $1, %ebx # stdout
-    movl $message, %ecx # buffer
-    movb $'A', message
-    movb $'B', 1(%ecx)
-    movb $'C', (%ecx, %ebx, 2)
-    movb $'D', message - 1(, %ebx, 4)
-    movb $'E', message(, %ebx, 4)
-
-    movl $(message_end - message), %edx # length
+    movl $(message_end - message), %edx # 参数3 缓冲区buf length
     int $0x80
 
-    movl $1, %eax # exit
-    movl $0, %ebx # status
+    movl $1, %eax # exit 调用号, 退出程序
+    movl $0, %ebx # status 状态
     int $0x80
 
 .section .data
